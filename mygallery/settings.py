9380f8f669274928c581c3428df07313c6c8d13e
Nkurumwa <nkurumwa51@gmail.com>
2019-11-11 14:53:57 +0300

    removes indentation errors

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # configuring the location for media
 MEDIA_URL = '/media/'
